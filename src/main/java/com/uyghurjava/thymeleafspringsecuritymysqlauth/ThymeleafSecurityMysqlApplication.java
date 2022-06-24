bd4082d301d0c598dbcaa5052a47cd7f7c088405
Subhi YD <subhy809fr@gmail.com>
2022-06-24 11:33:17 +0200

    v1.1 Rajout un fichier

---
+++ src/main/java/com/uyghurjava/thymeleafspringsecuritymysqlauth/ThymeleafSecurityMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.uyghurjava.thymeleafspringsecuritymysqlauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSecurityMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSecurityMysqlApplication.class, args);
+    }
+
+}
