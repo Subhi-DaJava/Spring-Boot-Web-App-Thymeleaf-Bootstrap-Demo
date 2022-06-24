7ae0956573bde94107653a097511995b66c02b5d
Subhi YD <subhy809fr@gmail.com>
2022-06-24 11:31:02 +0200

    V1 Créé Projet

---
+++ src/test/java/com/uyghurjava/thymeleafspringsecuritymysqlauth/ThymeleafSecurityMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyghurjava.thymeleafspringsecuritymysqlauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSecurityMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
